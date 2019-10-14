7d0c6c60482d409c5e28ad6af3c12cbdbbe9f5eb
LucasElisabeth <LpsElisabeth2001@gmail.com>
2019-10-14 23:38:17 +0200

    Started work on the keyboard but not quite sure if this is the correct way to go about it, will see more tomorrow

--- src/nl/codeforall/cannabits/hornpub/Game.java
+++ src/nl/codeforall/cannabits/hornpub/Game.java
@@ -1,12 +1,16 @@
 package nl.codeforall.cannabits.hornpub;
 
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
 public class Game implements KeyboardHandler {
 
     private Player player1;
     private Player player2;
+    private Keyboard keyboard;
+
 
     public Game(Player player1, Player player2) {
         this.player1 = player1;
@@ -14,8 +18,12 @@ public class Game implements KeyboardHandler {
     }
 
     public void start() {
-
+        keyboardMethodsPlayer1();
+        keyboardMethodsPlayer2();
     }
+
+
+
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
@@ -26,4 +34,47 @@ public class Game implements KeyboardHandler {
     public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
+
+    public void keyboardMethodsPlayer1() {
+        KeyboardEvent goLeft = new KeyboardEvent();
+        KeyboardEvent goRight = new KeyboardEvent();
+        KeyboardEvent goUp = new KeyboardEvent();
+        KeyboardEvent goDown = new KeyboardEvent();
+
+        goLeft.setKey(KeyboardEvent.KEY_LEFT);
+        goLeft.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goDown.setKey(KeyboardEvent.KEY_DOWN);
+        goDown.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goRight.setKey(KeyboardEvent.KEY_RIGHT);
+        goRight.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goUp.setKey(KeyboardEvent.KEY_UP);
+        goUp.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        keyboard.addEventListener(goDown);
+        keyboard.addEventListener(goLeft);
+        keyboard.addEventListener(goUp);
+        keyboard.addEventListener(goRight);
+    }
+
+    public void keyboardMethodsPlayer2() {
+        KeyboardEvent goLeft2 = new KeyboardEvent();
+        KeyboardEvent goRight2 = new KeyboardEvent();
+        KeyboardEvent goUp2 = new KeyboardEvent();
+        KeyboardEvent goDown2 = new KeyboardEvent();
+
+        goLeft2.setKey(KeyboardEvent.KEY_A);
+        goLeft2.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goDown2.setKey(KeyboardEvent.KEY_S);
+        goDown2.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goRight2.setKey(KeyboardEvent.KEY_D);
+        goRight2.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        goUp2.setKey(KeyboardEvent.KEY_W);
+        goUp2.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        keyboard.addEventListener(goDown2);
+        keyboard.addEventListener(goLeft2);
+        keyboard.addEventListener(goUp2);
+        keyboard.addEventListener(goRight2);
+    }
+
 }
--- src/nl/codeforall/cannabits/hornpub/Main.java
+++ src/nl/codeforall/cannabits/hornpub/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello hornpub players!");
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Game game = new Game(player1, player2);
+
+        game.start();
     }
 }
